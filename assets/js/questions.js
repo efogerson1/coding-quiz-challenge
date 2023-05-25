7aa52b5b5791124f94155e2bf6f304dadfce5fef
efogerson1 <efogerson2@gmail.com>
2023-05-25 15:18:44 -0700

    edited README and questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,23 +1,23 @@
 // Questions array
 var questions = [
   {
-    title: "Question 1",
-    choices: ["Choice A", "Choice B", "Choice C", "Choice D"],
-    answer: "Choice A"
+    title: "What does HTML stand for?",
+    choices: ["Hyper Text Markup Language", "Hyper Trainer Marking Language", "Hyper Text Markup Leveler", "Hyper Text Marketing Language"],
+    answer: "Hyper Text Markup Language"
   },
   {
-    title: "Question 2",
-    choices: ["Choice A", "Choice B", "Choice C", "Choice D"],
-    answer: "Choice B"
+    title: "Where is the JavaScript placed inside an HTML document or page?",
+    choices: ["In the <footer> section.", "In the <body> and <head> sections.", "In the <title> section.", "In the <meta> section."],
+    answer: "In the <body> and <head> sections."
   },
   {
-    title: "Question 3",
-    choices: ["Choice A", "Choice B", "Choice C", "Choice D"],
-    answer: "Choice C"
+    title: "In JavaScript, what is a block of code called that is used to perform a specific task?",
+    choices: ["Declaration", "Variable", "Function", "String"],
+    answer: "Function"
   },
   {
-    title: "Question 4",
-    choices: ["Choice A", "Choice B", "Choice C", "Choice D"],
+    title: "What tag is used to specify a section of text that has been quoted from another source?",
+    choices: ["<blockquote>", "<em>", "<strong>", "<a>"],
     answer: "Choice D"
   }
 ];
